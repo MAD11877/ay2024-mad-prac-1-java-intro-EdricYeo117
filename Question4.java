f06138b22134e2cac147ce5641a7e2d367ecd442
NPO-NP <78456508+NPO-NP@users.noreply.github.com>
2024-04-26 11:53:18 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,7 +18,7 @@ public class Question4
      * 
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in); 
     for (int i = in.nextInt(); i > 0; i--)
     {
       for (int j = i; j > 0; j--)
